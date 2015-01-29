e209b93c3b4eb03d7b653f7bdb523c5f7de1d29d
TAKANO Sho / @masaru_b_cl <masaru.b.cl@gmail.com>
2015-01-29 12:04:43 +0900

    windows環境向けに修正する

--- .bashrc
+++ .bashrc
@@ -3,5 +3,4 @@ alias now='git now'
 alias nowf='git now --fixup'
 alias gs='git st --short'
 alias gitk='gitk --all &'
-alias flow='git flow'
-alias vim=~/bin/vim73-kaoriya-win32/gvim.exe
+alias vim=gvim
---
+++ install.bat
@@ -0,0 +1,13 @@
+cd /d %~d0%~p0
+
+del %UserProfile%\.bashrc
+del %UserProfile%\.gitconfig
+del %UserProfile%\.gitignore_global
+del %UserProfile%\_vimrc
+del %UserProfile%\_gvimrc
+
+mklink /h %UserProfile%\.bashrc .bashrc
+mklink /h %UserProfile%\.gitconfig .gitconfig
+mklink /h %UserProfile%\.gitignore_global .gitignore_global
+mklink /h %UserProfile%\_vimrc _vimrc
+mklink /h %UserProfile%\_gvimrc _gvimrc
