7b11afce33ad794b2d07c918b122a28bf58355dd
TAKANO Sho / @masaru_b_cl <masaru.b.cl@gmail.com>
2016-09-02 11:27:10 +0900

    Vim関連設定を追加する

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
 ln -s ~/gitrepos/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/gitrepos/dotfiles/.gitignore_global ~/.gitignore_global
 ln -s ~/gitrepos/dotfiles/.vimrc ~/.vimrc
+
+mkdir ~/.vim
+mkdir ~/.vim/bundle
+mkdir ~/.vim/backup
+mkdir ~/.vim/undo
+mkdir ~/.vim/swp
+
+curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
